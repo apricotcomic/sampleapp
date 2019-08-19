b8b1ef0d6b93dfde934c5bad1ac66dcabdd1141a
apricotcomic <apricotcomic@gmail.com>
2019-08-19 22:49:05 +0900

    自力で認証機能を作る　その１

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,17 @@
     <meta charset='utf-8'>
 </head>
 <body>
-<h1>ログインフォーム</h1>
+<h1>ログイン</h1>
+<!--- エラーメッセージをここに表示します -->
 @isset($message)
-    <p style="coloer:red">{{$message}}</p>
+    <p style="color:red">{{$message}}</p>
 @endisset
+<!-- フォーム -->
 <form name="loginform" action="/auth/login" method="post">
-    {{ csrf_field() }}
-    メールアドレス：<input type="text" name="email" size="30" value="{{ old('email') }}"><br />
-    パスワード：<input type="password" name="password" size="30"><br />
+    <!--- csrf対応 -->
+    {{ csrf_field }}
+    ユーザーID:<input type="text" name="id" size="10" value="{{ old('id') }}"><br />
+    パスワード:<input type="password" name="password" size="30"><br />
     <button type='submit' name='action' value='send'>ログイン</button>
 </form>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('auth/register', 'Auth\RegisterController@register');
 
 Route::get('/auth/login', 'Auth\LoginController@showLoginForm');
-Route::post('/auth/login', 'Auth\LoginController@Login');
+Route::post('/auth/login', 'Auth\LoginController@login');
 
-Route::get('/auth/logout', 'Auth\Logincontroller@logout');
+Route::get('/auth/logout', 'Auth\LoginController@logout');
